7f63931bd8caf13ffeb911da6814b8c3e2a38ea6
Madina <madinaakpaeva000@gmail.com>
2023-10-18 22:18:54 +0600

    added fragments and colors

--- app/src/main/java/com/example/drivingschool/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/drivingschool/ui/activity/MainActivity.kt
@@ -2,13 +2,45 @@ package com.example.drivingschool.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.drivingschool.R
+import com.example.drivingschool.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navView = binding.navView
+        setSupportActionBar(binding.myToolbar)
+        setAppBar()
+    }
+
+    private fun setAppBar() {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.enrollFragment,
+                R.id.profileFragment,
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
