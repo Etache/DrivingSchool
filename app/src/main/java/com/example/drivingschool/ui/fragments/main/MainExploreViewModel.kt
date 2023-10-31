de3cd359f0a71ee7d411e3babbf8afb8ea8b959e
KUBA <begalikurmanbek@gmail.com>
2023-10-31 17:14:09 +0600

    bug fix

--- app/src/main/java/com/example/drivingschool/ui/fragments/main/MainExploreViewModel.kt
+++ app/src/main/java/com/example/drivingschool/ui/fragments/main/MainExploreViewModel.kt
@@ -3,4 +3,5 @@ package com.example.drivingschool.ui.fragments.main
 import com.example.drivingschool.base.BaseViewModel
 
 class MainExploreViewModel: BaseViewModel() {
+
 }
--- app/src/main/java/com/example/drivingschool/ui/fragments/mainDetails/CurrentLessonDetailsFragment.kt
+++ app/src/main/java/com/example/drivingschool/ui/fragments/mainDetails/CurrentLessonDetailsFragment.kt
@@ -1,16 +1,73 @@
 package com.example.drivingschool.ui.fragments.mainDetails
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.drivingschool.R
 import com.example.drivingschool.base.BaseFragment
 import com.example.drivingschool.databinding.FragmentCurrentLessonDetailBinding
 
-class CurrentLessonDetailsFragment() :
+class CurrentLessonDetailsFragment :
     BaseFragment<FragmentCurrentLessonDetailBinding, CurrentLessonDetailsViewModel>(R.layout.fragment_current_lesson_detail) {
 
     override val binding by viewBinding(FragmentCurrentLessonDetailBinding::bind)
-    override val viewModel: CurrentLessonDetailsViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel: CurrentLessonDetailsViewModel by viewModels()
 
+    override fun initialize() {
+
+
+    }
+
+    override fun setupListeners() {
+        binding.btnCancelLesson.setOnClickListener{
+            showCustomDialog()
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private fun showCustomDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        val customDialog = LayoutInflater.from(requireContext()).inflate(R.layout.custom_rate_dialog, null)
+        builder.setView(customDialog)
+
+        val rating = customDialog.findViewById<RatingBar>(R.id.rb_comment_small)
+        val edt = customDialog.findViewById<EditText>(R.id.et_comment_text)
+        val counter = customDialog.findViewById<TextView>(R.id.tv_comment_character_count)
+        val btn = customDialog.findViewById<Button>(R.id.btn_comment_confirm)
+
+        edt.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                counter.text = "(${p0?.length.toString()}/250)"
+//          counter.text = getString(R.string._0_250,p0?.length.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        }
+        )
+
+        btn.setOnClickListener {
+            rating.setOnRatingBarChangeListener { ratingBar, _, _ ->
+                counter.text = "${ratingBar.rating}"
+            }
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
 
 }
