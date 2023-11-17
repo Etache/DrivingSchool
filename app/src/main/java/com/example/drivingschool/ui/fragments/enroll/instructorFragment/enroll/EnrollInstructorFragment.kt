08a6b752046bb36e304970f96e57a46edbfb16dd
Nothingisnotnothing <somethingforthismail@gmail.com>
2023-11-17 13:59:27 +0600

    Возвращение назад в EnrollInstructorFragment

--- app/src/main/java/com/example/drivingschool/ui/fragments/enroll/instructorFragment/enroll/EnrollInstructorFragment.kt
+++ app/src/main/java/com/example/drivingschool/ui/fragments/enroll/instructorFragment/enroll/EnrollInstructorFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -28,7 +29,12 @@ class EnrollInstructorFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
     }
 
     override fun initialize() {
