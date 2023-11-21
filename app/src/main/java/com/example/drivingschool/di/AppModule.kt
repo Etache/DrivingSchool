444a116d84a547778f39f49271af782864594e68
nurzida <nurzidaulanbekovna@gmail.com>
2023-11-21 09:07:11 +0300

    added NotificationFragment

--- app/src/main/java/com/example/drivingschool/di/AppModule.kt
+++ app/src/main/java/com/example/drivingschool/di/AppModule.kt
@@ -23,7 +23,7 @@ object AppModule {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): DrivingApiService {
         return Retrofit.Builder()
-            .baseUrl("https://529b-94-143-198-249.ngrok-free.app/")
+            .baseUrl("https://b0b8-94-143-197-63.ngrok-free.app/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
