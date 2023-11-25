27eb9b8dd65a5312ae9630ee0c9e5300040df0ae
Madina <madinaakpaeva000@gmail.com>
2023-11-25 15:50:29 +0600

    cleanded enroll

--- app/src/main/java/com/example/drivingschool/ui/fragments/enroll/confirmEnroll/EnrollFragment.kt
+++ app/src/main/java/com/example/drivingschool/ui/fragments/enroll/confirmEnroll/EnrollFragment.kt
@@ -58,7 +58,7 @@ class EnrollFragment :
                         }
 
                         else -> {
-                            showToast("enroll")
+                            showToast("")
                         }
                     }
                 }
